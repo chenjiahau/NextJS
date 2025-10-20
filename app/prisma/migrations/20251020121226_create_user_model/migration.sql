04a57bc7fbec840f1a46465325bce7613076d936
ivanchen <ivan.chen@dlinkcorp.com>
2025-10-20 20:13:08 +0800

    Fix migration step 1. If migrating from scratch:    npx prisma migrate dev --name message 2. If recreating the database:    npx prisma migrate deploy

--- app/prisma/migrations/20251020121226_create_user_model/migration.sql
+++ app/prisma/migrations/20251020121226_create_user_model/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
+    "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
